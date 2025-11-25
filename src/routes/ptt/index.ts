bff05e4dcc62f296a60a0eb14715e74fe4beeb94
Tirawat Owen <bamiowen@gmail.com>
2025-11-25 22:59:13 +0700

    complete ptt invoice total demand gulf

--- src/routes/ptt/index.ts
+++ src/routes/ptt/index.ts
@@ -1,15 +1,19 @@
 import Elysia from "elysia";
+import { invoiceRoutes } from "./invoice";
 import { lngRoutes } from "./lng";
 import { lpgRoutes } from "./lpg";
 import { pttSupplyRoutes } from "./ptt-supply";
 import { supplyRoutes } from "./supply";
 import { tsoRoutes } from "./tso";
 
-export const pttRoutes = new Elysia().group("/ptt", (c) =>
+export const pttRoutes = new Elysia().group(
+    "/ptt",
+    (c) =>
     c
-        .use(supplyRoutes)
-        .use(pttSupplyRoutes)
-        .use(tsoRoutes)
-        .use(lngRoutes)
-        .use(lpgRoutes)
+            .use(supplyRoutes)
+            .use(pttSupplyRoutes)
+            .use(tsoRoutes)
+            .use(lngRoutes)
+            .use(lpgRoutes)
+            .use(invoiceRoutes)
 );
